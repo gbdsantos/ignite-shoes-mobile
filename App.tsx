d8fe0f340dacced5bfd196c96e800901082baf7c
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-05-13 00:55:09 -0300

    ✍️ update(mobile): use tags to identify the user

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar } from 'react-native';
+import { Platform, StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import OneSignal from 'react-native-onesignal';
 
@@ -10,11 +10,20 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+import { tagUserEmailCreate } from './src/notifications/notificationsTags';
 
-OneSignal.setAppId(`${process.env.ONESIGNAL_APP_ID}`);
+const oneSignalAppId = Platform.OS === 'ios' ? '' : `${process.env.ONESIGNAL_APP_ID}`;
+
+OneSignal.setAppId(oneSignalAppId);
+
+OneSignal.promptForPushNotificationsWithUserResponse(response => {
+  console.log(response);
+});
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  tagUserEmailCreate('gbsantos@gmail.com');
 
   return (
     <NativeBaseProvider theme={THEME}>
