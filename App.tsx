08c3f1e0c4b7c85ab04bdf85da3df11780e91c88
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-05-13 00:04:09 -0300

    ✍️ update(mobile): add onesignal

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
+import OneSignal from 'react-native-onesignal';
+
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Routes } from './src/routes';
@@ -8,6 +10,8 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.setAppId(`${process.env.ONESIGNAL_APP_ID}`);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
