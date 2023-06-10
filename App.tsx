035c7972da62d93ffa8d92755c19d2008ed5b8b2
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-06-09 22:28:29 -0300

    ✍️ update(mobile): receive message in foreground

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
+import { useEffect } from 'react';
 import { Platform, StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
-import OneSignal from 'react-native-onesignal';
+import OneSignal, { NotificationReceivedEvent } from 'react-native-onesignal';
 
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -11,19 +12,27 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 import { tagUserInfoCreate } from './src/notifications/notificationsTags';
+import { Notification } from './src/components/Notification';
 
 const oneSignalAppId = Platform.OS === 'ios' ? '' : `${process.env.ONESIGNAL_APP_ID}`;
 
 OneSignal.setAppId(oneSignalAppId);
 
-OneSignal.promptForPushNotificationsWithUserResponse(response => {
-  console.log(response);
-});
+OneSignal.promptForPushNotificationsWithUserResponse();
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   tagUserInfoCreate();
+
+  useEffect(() => {
+    const unsubscribe = OneSignal
+      .setNotificationWillShowInForegroundHandler((notificationReceivedEvent: NotificationReceivedEvent) => {
+        console.log(notificationReceivedEvent);
+      });
+
+    return () => unsubscribe;
+  }, []);
 
   return (
     <NativeBaseProvider theme={THEME}>
