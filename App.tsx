914ab31c2ebd250876421c92f5d4993a6db71cf7
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-06-16 00:24:24 -0300

    ♻️ refactor(mobile): refactor notification component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,6 @@
-import { useEffect, useState } from 'react';
 import { Platform, StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
-import OneSignal, {
-  NotificationReceivedEvent,
-  OSNotification
-} from 'react-native-onesignal';
+import OneSignal from 'react-native-onesignal';
 
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -15,7 +11,6 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 import { tagUserInfoCreate } from './src/notifications/notificationsTags';
-import { Notification } from './src/components/Notification';
 
 const oneSignalAppId = Platform.OS === 'ios' ? '' : `${process.env.ONESIGNAL_APP_ID}`;
 
@@ -25,20 +20,8 @@ OneSignal.promptForPushNotificationsWithUserResponse();
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
-  const [notification, setNotification] = useState<OSNotification | undefined>(undefined);
 
   tagUserInfoCreate();
-
-  useEffect(() => {
-    const unsubscribe = OneSignal
-      .setNotificationWillShowInForegroundHandler((notificationReceivedEvent: NotificationReceivedEvent) => {
-        const response = notificationReceivedEvent.getNotification();
-
-        setNotification(response);
-      });
-
-    return () => unsubscribe;
-  }, []);
 
   return (
     <NativeBaseProvider theme={THEME}>
@@ -50,13 +33,6 @@ export default function App() {
       <CartContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
       </CartContextProvider>
-
-      {notification?.title &&
-        <Notification
-          title={notification.title}
-          onClose={() => setNotification(undefined)}
-        />
-      }
     </NativeBaseProvider>
   );
 }
--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,4 +1,13 @@
-import { HStack, Text, IconButton, CloseIcon, Icon } from 'native-base';
+import {
+  CloseIcon,
+  HStack,
+  Icon,
+  IconButton,
+  Pressable,
+  Text
+} from 'native-base';
+import { useNavigation } from '@react-navigation/native';
+
 import { Ionicons } from '@expo/vector-icons';
 
 type Props = {
@@ -7,31 +16,40 @@ type Props = {
 }
 
 export function Notification({ title, onClose }: Props) {
+  const { navigate } = useNavigation();
+
+  function handleOnPress() {
+    navigate('details', { productId: '7' });
+    onClose();
+  }
+
   return (
-    <HStack 
-      w="full" 
-      p={4} 
-      pt={12}
-      justifyContent="space-between" 
-      alignItems="center" 
+    <Pressable
       bgColor="gray.200"
+      onPress={handleOnPress}
       position="absolute"
+      p={4}
+      pt={12}
       top={0}
+      w="full"
     >
-        <Icon as={Ionicons} name="notifications-outline" size={5} color="black" mr={2}/>
+      <HStack
+        justifyContent="space-between" alignItems="center">
+        <Icon as={Ionicons} name="notifications-outline" size={5} color="black" mr={2} />
 
         <Text fontSize="md" color="black" flex={1}>
           {title}
         </Text>
 
-      <IconButton 
-        variant="unstyled" 
-        _focus={{ borderWidth: 0 }} 
-        icon={<CloseIcon size="3" />} 
-        _icon={{ color: "coolGray.600"}} 
-        color="black"
-        onPress={onClose}
-      />
-    </HStack>
+        <IconButton
+          variant="unstyled"
+          _focus={{ borderWidth: 0 }}
+          icon={<CloseIcon size="3" />}
+          _icon={{ color: "coolGray.600" }}
+          color="black"
+          onPress={onClose}
+        />
+      </HStack>
+    </Pressable>
   );
 }
