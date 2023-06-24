f8ee3b8552ddafa1274f8bb652706ce4df1c2026
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-06-23 23:35:15 -0300

    ✨ feat(mobile): handle deep linking with app in foreground

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -6,8 +6,8 @@ import {
   Pressable,
   Text
 } from 'native-base';
-import { useNavigation } from '@react-navigation/native';
 
+import * as Linking from 'expo-linking';
 import { Ionicons } from '@expo/vector-icons';
 import { OSNotification } from 'react-native-onesignal';
 
@@ -16,21 +16,10 @@ type Props = {
   onClose: () => void;
 }
 
-type AdditionalDataProps = {
-  route?: 'details';
-  product_id?: string;
-}
-
 export function Notification({ data, onClose }: Props) {
-  const { navigate } = useNavigation();
-
-  console.log(data);
-
   function handleOnPress() {
-    const { route, product_id } = data.additionalData as AdditionalDataProps;
-
-    if (route === 'details' && product_id) {
-      navigate('details', { productId: product_id });
+    if (data.launchURL) {
+      Linking.openURL(data.launchURL);
       onClose();
     }
   }
